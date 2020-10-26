70f40f9ac11dcb5624b484bdfacc0a45275674a4
unknown <you@example.com>
2020-10-26 15:47:57 +0300

    Updates

---
+++ precache-manifest.751a600096c4debe4ebac6c2d47b68c8.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cacd85e1630f51d7badffee290145748",
+    "url": "/obolonsite/index.html"
+  },
+  {
+    "revision": "7d90498bab7c165e5920",
+    "url": "/obolonsite/static/css/main.9ab976ac.chunk.css"
+  },
+  {
+    "revision": "14be2a3cf7e044a34071",
+    "url": "/obolonsite/static/js/2.2a94769f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/obolonsite/static/js/2.2a94769f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d90498bab7c165e5920",
+    "url": "/obolonsite/static/js/main.00a060a6.chunk.js"
+  },
+  {
+    "revision": "62898fedac0d2a1f57ae",
+    "url": "/obolonsite/static/js/runtime-main.825100b8.js"
+  },
+  {
+    "revision": "f077f143e0f40a5e2637431f68592570",
+    "url": "/obolonsite/static/media/Zhyguli-logo.f077f143.png"
+  },
+  {
+    "revision": "09352e0bd13b0b1bb9fbce9c63c4e718",
+    "url": "/obolonsite/static/media/beer-bottle.09352e0b.png"
+  },
+  {
+    "revision": "9ec3ae1c609ff92ab87a687f6460c914",
+    "url": "/obolonsite/static/media/beer-hike.9ec3ae1c.png"
+  },
+  {
+    "revision": "0587492831a62e0634170c49200919cf",
+    "url": "/obolonsite/static/media/beer_hike_blue.05874928.png"
+  },
+  {
+    "revision": "4623b369ab376f56ccdc886a23c581b9",
+    "url": "/obolonsite/static/media/beer_magnat.4623b369.png"
+  },
+  {
+    "revision": "311f3ed389ff3250c3b7f55ac8a5a33f",
+    "url": "/obolonsite/static/media/beer_obolon_premium.311f3ed3.png"
+  },
+  {
+    "revision": "22e87f47ba6eef6cb079587bc798d903",
+    "url": "/obolonsite/static/media/beer_obolon_svitle.22e87f47.png"
+  },
+  {
+    "revision": "dd77110e5f662eda31e1ea010e6ba3c1",
+    "url": "/obolonsite/static/media/beer_range.dd77110e.png"
+  },
+  {
+    "revision": "910170a261edb9eddc322d990555be25",
+    "url": "/obolonsite/static/media/beer_zhygulivske.910170a2.png"
+  },
+  {
+    "revision": "70dec872974a303f17a733f26b784756",
+    "url": "/obolonsite/static/media/beer_zibert.70dec872.png"
+  },
+  {
+    "revision": "c1d4d853a8b1129e3a70fcf5497cbbef",
+    "url": "/obolonsite/static/media/beer_zibert_can.c1d4d853.png"
+  },
+  {
+    "revision": "9518d44a6e6653dafa3e7dad1618023c",
+    "url": "/obolonsite/static/media/factory.9518d44a.png"
+  },
+  {
+    "revision": "84da95b4270a580f262361a54e2f745e",
+    "url": "/obolonsite/static/media/kwas_chlebowy.84da95b4.png"
+  },
+  {
+    "revision": "7f14d979594de0872c95d4554abe0592",
+    "url": "/obolonsite/static/media/kwas_podpiwek.7f14d979.png"
+  },
+  {
+    "revision": "afceb09913e56c15d9f05d5e15ed9aff",
+    "url": "/obolonsite/static/media/kwas_sliwa.afceb099.png"
+  },
+  {
+    "revision": "02f71899d13ed1320c884071b4a6b5cd",
+    "url": "/obolonsite/static/media/kwas_wisznia.02f71899.png"
+  },
+  {
+    "revision": "b0f83db39cc4a0643dbdd7bf37284989",
+    "url": "/obolonsite/static/media/kyiv.b0f83db3.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/obolonsite/static/media/logo.5d5d9eef.svg"
+  }
+]);
